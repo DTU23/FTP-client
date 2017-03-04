ab8951be2146469191543fdebb30ed073061a9ce
Viktor Poulsen <viktormaigaard@gmail.com>
2017-03-04 23:35:16 +0100

    added comments

--- src/main.cpp
+++ src/main.cpp
@@ -28,24 +28,26 @@ int main( int argc, char** argv) {
                                   "   | )  \\  || (____/\\   | |     \\   /  | )   ( || (____/\\| ) \\ \\__|  /  \\ \\/\\____) || )      | ) \\ \\__| )   ( ||  /  \\ \\   | |   ___) (___|  /  \\ \\\n"
                                   "   |/    )_)(_______/   )_(      \\_/   |/     \\|(_______/|/   \\__/|_/    \\/\\_______)|/       |/   \\__/|/     \\||_/    \\/   )_(   \\_______/|_/    \\/\n"
                                   "                                                                                                                                                   ");
-    // Instantiate FTPClient as anonymous user in passive mode
+
+    // Instantiate FTPClient as anonymous user using blank password
     FTPClient ftpClient(SERVER_IP, FTP_PORT, "anonymous", "");
 
     bool run = true;
     char menu_choice[256];
     print_menu();
+
     while(run)
     {
         assert(fgets(menu_choice, 256, stdin) != NULL);
-        // Verifies that the previous character to the last character in the
-        // buffer array is '\n' (The last character is '\0') if the
-        // character is '\n' leaves loop.
-        if(menu_choice[strlen(menu_choice) - 1] == '\n')
-        {
-            // fgets reads and adds '\n' in the string, replace '\n' by '\0' to
-            // remove the line break .
+        // Detect enter-button
+        if(menu_choice[strlen(menu_choice) - 1] == '\n') {
+            // changing last \n to \0 to remove line break
             menu_choice[strlen(menu_choice) - 1] = '\0';
+            // Treat menu choice as integer
             switch (atoi(menu_choice)){
+                /**
+                 * Choice 1 will download the small file.txt from the root-folder
+                 */
                 case 1:{
                     ftpClient.enter_passive_mode();
                     DataSocket DataSocket(SERVER_IP, ftpClient.get_data_port_number());
@@ -59,6 +61,9 @@ int main( int argc, char** argv) {
                     Helper::print_message(ftpClient.get_response());
                     break;
                 }
+                /**
+                 * Choice 2 will download the larger pdf-file from the /pub/62501/ folder
+                 */
                 case 2:{
                     ftpClient.enter_passive_mode();
                     DataSocket DataSocket(SERVER_IP, ftpClient.get_data_port_number());
@@ -72,6 +77,9 @@ int main( int argc, char** argv) {
                     Helper::print_message(ftpClient.get_response());
                     break;
                 }
+                /**
+                 * Choice 3 will attempt uploading a file
+                 */
                 case 3:{
                     ftpClient.enter_passive_mode();
                     DataSocket DataSocket(SERVER_IP, ftpClient.get_data_port_number());
@@ -83,21 +91,34 @@ int main( int argc, char** argv) {
                     DataSocket.send_file("send.txt");
                     break;
                 }
+                /**
+                 * Choice 4 will print file contents
+                 */
                 case 4:
                     printFileCont("/home/viktor/Desktop/file.txt");
                     break;
+                /**
+                 * Choice 5 will stop the loop end control sequence will come to an end
+                 */
                 case 5:
                     run = false;
                     break;
+                /**
+                 * Defaul will catch all non-valid menu-choices
+                 */
                 default:
                     Helper::print_message("invalid choice!");
                     break;
             }
         }
+        // Re-print menu before asking for a new input
         print_menu();
     }
 }
 
+/**
+ * Method to print our ftp-client-menu
+ */
 void print_menu(){
     Helper::print_message(" ___  ___  ___   ___  _  _             _   \n"
                                   "| __>|_ _|| . \\ |  _>| |<_> ___ ._ _ _| |_ \n"
